03d95b9f4436067fce9afd24a1228fe812bc2a08
Charmainetannlb <53883843+Charmainetannlb@users.noreply.github.com>
2024-05-14 04:29:28 +0000

    Death button

---
+++ script.js
@@ -0,0 +1,35 @@
+var age = 65
+
+console.log(age)
+
+var username ="death"
+var password = "techup"
+
+var isMale = false
+var isLoggedIn = false
+
+var blog_posts = ["Today was tiring", "Today was Death", "Today was Dr Pawson"]
+
+console.log(age)
+console.log(username)
+console.log(password)
+console.log(isMale)
+console.log(isLoggedIn)
+console.log(blog_posts)
+
+if (isMale ==true) {
+    console.log("You are male");
+} else {
+    console.log("you are not male");
+}
+
+while (age < 70) {
+    console.log("Your age is " + age );
+    age = age + 1;
+}
+
+function sayHello() {
+    alert("7 days until you die");
+}
+
+sayHello();
